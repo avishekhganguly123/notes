0b88d6a688e748247dc135ed4f214765fc8c1575
Avishekh Ganguly <abishekganguly123@gmail.com>
2022-07-28 23:42:47 +0530

    flatten binary tree

---
+++ notes.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void flatten(TreeNode* root) {
+         if(root==NULL)
+            return;
+        stack<TreeNode*>st;
+        st.push(root);
+        TreeNode* cur=NULL;
+        while(!st.empty()){
+            cur=st.top();
+            st.pop();
+            if(cur->right) st.push(cur->right);
+            if(cur->left) st.push(cur->left);
+             if(!st.empty()){
+                 cur->right=st.top();
+                 cur->left=NULL;
+             }
+           }
+        return;
+ 
+    }
+    
+    //
+       // if(root==NULL)
+       //      return;
+       //  stack<TreeNode*>stack;
+       //  stack.push(root);
+       //  while(!stack.empty()){
+       //      TreeNode*currentNode=stack.top();
+       //      stack.pop();
+       //      if(currentNode->right!=NULL)
+       //          stack.push(currentNode->right);
+       //       if(currentNode->left!=NULL)
+       //          stack.push(currentNode->left);
+       //       if(!stack.empty())
+       //           currentNode->right=stack.top();
+       //      currentNode->left=NULL;
+       //  }
+
+};
